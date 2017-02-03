f0deb32de3775fdb39b99c6f58fce4e034bbf5a0
drichard1989 <drichard1989@icloud.com>
2017-02-02 19:53:01 -0600

    Updating the server.js file with the variable for the array that will hold the objects being pushed into the array.

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require ("express");
+var path = require ("path");
+var bodyParser = require ("body-parser");
+
+
+var app = express();
+var PORT = 3000;
+
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+
+var guestList = [];
+app.post("/api/reservation", function (req, res){
+  var newcharacter = req.body;
+
+  res.sendFile(path.join(__dirname, "reservation.html"))
+})
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
